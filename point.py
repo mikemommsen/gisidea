5385fbf4b03a6b7669f8e4e94fa36c2f952a6ad4
mikemommsen <mikemommsen@gmail.com>
2013-03-26 13:28:24 -0500

    Update point.py

--- point.py
+++ point.py
@@ -1,4 +1,4 @@
-
+import math
 class Point:
     def __init__(self,x=None,y=None,z=None,t=None):
         self.x = x
@@ -11,4 +11,62 @@ class Point:
 
 
     def distance(self, other):
+        return math.hypot(self.x - other.x, self.y - other.y)
+        
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+        
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+        
+    def __mul__(self, other):
+        return Point(self.x * other.x, self.y * other.y)
+
+    def __floordiv__(self, other):
+        return Point(self.x // other.x, self.y // other.y)
+        
+    def __mod__(self, other):
+        return Point(self.x % other.x, self.y % other.y)
+
+    def __divmod__(self, other):
+        # this is not doable i dont think
+        return Point(self.x - other.x, self.y + other.y)
+        
+    def __pow__(self, other[, modulo]):
+        return Point(pow(self.x, other.x), pow(self.y, other.y))
+
+    def __div__(self, other):
+        return Point(self.x / other.x, self.y / other.y)
+
+    def __radd__(self, other):
+        return Point(self.x + other, self.y + other)
+        
+    def __rsub__(self, other):
+        return Point(self.x - other, self.y - other)
+        
+    def __rmul__(self, other):
+        return Point(self.x * other, self.y * other)
+        
+    def __iadd__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+        
+    def __isub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+        
+    def __imul__(self, other):
+        return Point(self.x * other.x, self.y * other.y)
+
+    def __neg__(self):
+        return Point(neg(self.x), neg(self.y))
+
+    def __pos__(self):
+        return Point(pos(self.x), pos(self.y))
+        
+
+    def __abs__(self):
+        return Point(abs(self.x), abs(self.y))
+
+    def __invert__(self):
+        return Point(invert(self.x), invert(self.y))
+
 
