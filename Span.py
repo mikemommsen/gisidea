e3b59b53f4457c37a0dcd0d056969dfc57785039
mikemommsen <mikemommsen@gmail.com>
2013-03-26 20:06:27 -0500

    Update Span.py

--- Span.py
+++ Span.py
@@ -23,12 +23,38 @@ class Span:
         else:
             return False
     
-    def spanInside(self, other):
+    def disjoint(self, other):
+        if self.low > other.high or self.high < other.low:
+            return True
+        else:
+            return False
+            
+    def overlapping(self, other):
+        if self.pointInside(other.low) and self.pointOutside(other.high):
+            return True
+        elif self.pointOutside(other.low) and self.pointInside(other.high):
+            return True
+        else:
+            return False
+            
+    def nested(self, other):
+        if self.low < other.low and self.high > other.high:
+            return True
+        else:
+            return False
+            
+    def equal(self, other):
+        if self.low == other.low and self.high == other.high:
+            return True
+        else:
+            return False
+    
+    def touchesOutside(self, other):
         if pointInside(self, other.low) and pointInside(self, other.high):
             return True
         else:
             return False
             
-    def touches(self, other):
+    def touchesInside(self, other):
         if inside(self, other.low) or inside(self, other.high):
             return False
