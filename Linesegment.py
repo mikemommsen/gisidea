5d89e56afece6dcb45dc09d68cfd99056f9b1411
mikemommsen <mikemommsen@gmail.com>
2013-03-27 17:32:13 -0500

    Update Linesegment.py

--- Linesegment.py
+++ Linesegment.py
@@ -1,7 +1,20 @@
-import math
+import math, Point
 
 class Linesegment:
+    
+    def __init__(self, origin, end):
+        self.origin = origin
+        self.end = end
+        self.diff = origin - end
+        self.length = origin.distance(end)
+        
+    def calcAngle(self):
+        self.slope = self.diff.y / self.diff.x
+        self.atan2 = math.atan2(self.diff.y, self.diff.x)
+        self.atan = math.atan(self.slope)
+        
   
-  def isleft(self, inpoint):
-  def isright(self, inpoint):
+    def isleft(self, inpoint):
+        
+    def isright(self, inpoint):
     
