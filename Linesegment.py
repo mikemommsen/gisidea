b4819b74f17813f44bdf55a93b500edd996cedfe
mikemommsen <mikemommsen@gmail.com>
2013-03-29 13:53:10 -0500

    Update Linesegment.py

--- Linesegment.py
+++ Linesegment.py
@@ -11,7 +11,7 @@ class Linesegment:
     def calcAngle(self):
         self.slope = self.diff.y / self.diff.x
         self.atan2 = self.orgin.atan2(self.end)
-        self.atan = math.atan(self.slope)
+        self.atan = math.atan2(self.slope)
         
   
     def isleft(self, inpoint):
