0eeb694a4f93937759d10bdbee93be5868969055
mikemommsen <mikemommsen@gmail.com>
2014-01-07 11:19:07 -0600

    Update Tree.py

--- Tree.py
+++ Tree.py
@@ -1,2 +1,28 @@
 
+class Node(object):
+    """"""
+    def __init__(self, parent=None, children=None, oid=None):
+        """"""
+        self.oid=oid
+        self.parent=parent
+        self.children=children
 
+    def __str__(self):
+        basestring = 'Node object oid: {0}, parent: {1}, children: {2}'
+        return basestring.format(self.oid, self.parent, self.children)
+        
+class Tree(object):
+    """"""
+    def __init__(self, inlist=None, max_levels=None):
+        inlist = sorted(inlist)
+        medianIndex = len(inlist) / 2
+        median = inlist[medianIndex]
+        
+        
+    def rotate(self):
+        
+        
+        
+    
+    
+  
